37626919d2dbe1c3db3af471ef36958f433b72a8
JoaoPMLima <62726968+JoaoPMLima@users.noreply.github.com>
2020-04-17 22:19:18 -0300

    Update 2.0
    Changed the function if y/x ratio is bigger than 1
    Added hyperlink to desmos
    Improved instructions layout

--- Graphs/createFunction.js
+++ Graphs/createFunction.js
@@ -9,15 +9,17 @@ function createFunction(string,zz){
     //declarations
     let tempString = "";
     let array = [];
-
+    let moreThan45 = false;
     //solving array issues
-    array[0] = arrayOrig[0];
-    array[1] = arrayOrig[2];
-    array[2] = 500 - arrayOrig[1];
-    array[3] = 500 - arrayOrig[3];
+    //arrayOrig = [x1,y1,x2,y2]
+    array[0] = arrayOrig[0]; //x1
+    array[1] = arrayOrig[2]; //x2
+    array[2] = 500 - arrayOrig[1]; //y1
+    array[3] = 500 - arrayOrig[3]; //y2
     //z = this.zz*Math.abs((array[3] - array[2])/(array[1] - array[0]));
     z = "z";
     console.log(z);
+    //array is currently [x1,x2,y1,y2]
     if(array[0] < array[1]){
       array.push(array[0]);
       array.push(array[1]);
@@ -32,8 +34,12 @@ function createFunction(string,zz){
       array.push(array[3]);
       array.push(array[2]);
     }
+    //array is currently [x1,x2,y1,y2,xm,xM,ym,yM]
 
-
+    //if deltaY is bigger than deltaX set moreThan45 to true
+    if((array[7]-array[6]) > (array[5] - array[4])){
+      moreThan45 = true;
+    }
 
     //start
     tempString = tempString + "\\left(";
@@ -56,27 +62,63 @@ function createFunction(string,zz){
     //function-boundary limitations
 
     tempString = tempString + "\\left(\\frac{\\left|";
-    tempString = tempString + this.theFunct(array,false);
-    tempString = tempString + "-y+";
+    tempString = tempString + this.theFunct(array,false,moreThan45);
+    tempString = tempString + "-";
+    if(moreThan45){
+      tempString = tempString + "x";
+    } else {
+      tempString = tempString + "y";
+    }
+    tempString = tempString + "+";
     tempString = tempString + z;
     tempString = tempString + "\\right|+\\left(";
-    tempString = tempString + this.theFunct(array,false);
-    tempString = tempString + "-y+";
+    tempString = tempString + this.theFunct(array,false,moreThan45);
+    tempString = tempString + "-";
+    if(moreThan45){
+      tempString = tempString + "x";
+    } else {
+      tempString = tempString + "y";
+    }
+    tempString = tempString + "+";
     tempString = tempString + z;
     tempString = tempString + "\\right)}{2\\left(";
-    tempString = tempString + this.theFunct(array,false);
-    tempString = tempString + "-y+";
+    tempString = tempString + this.theFunct(array,false,moreThan45);
+    tempString = tempString + "-";
+    if(moreThan45){
+      tempString = tempString + "x";
+    } else {
+      tempString = tempString + "y";
+    }
+    tempString = tempString + "+";
     tempString = tempString + z;
-    tempString = tempString + "\\right)}\\right)\\left(\\frac{\\left|y-\\left(";
-    tempString = tempString + this.theFunct(array,false);
+    tempString = tempString + "\\right)}\\right)\\left(\\frac{\\left|";
+    if(moreThan45){
+      tempString = tempString + "x";
+    } else {
+      tempString = tempString + "y";
+    }
+    tempString = tempString + "-\\left(";
+    tempString = tempString + this.theFunct(array,false,moreThan45);
     tempString = tempString + "\\right)+";
     tempString = tempString + z;
-    tempString = tempString + "\\right|+\\left(y-\\left(";
-    tempString = tempString + this.theFunct(array,false);
+    tempString = tempString + "\\right|+\\left(";
+    if(moreThan45){
+      tempString = tempString + "x";
+    } else {
+      tempString = tempString + "y";
+    }
+    tempString = tempString + "-\\left(";
+    tempString = tempString + this.theFunct(array,false,moreThan45);
     tempString = tempString + "\\right)+";
     tempString = tempString + z;
-    tempString = tempString + "\\right)}{2\\left(y-\\left(";
-    tempString = tempString + this.theFunct(array,false);
+    tempString = tempString + "\\right)}{2\\left(";
+    if(moreThan45){
+      tempString = tempString + "x";
+    } else {
+      tempString = tempString + "y";
+    }
+    tempString = tempString + "-\\left(";
+    tempString = tempString + this.theFunct(array,false,moreThan45);
     tempString = tempString + "\\right)+";
     tempString = tempString + z;
     tempString = tempString + "\\right)}\\right)";
@@ -99,7 +141,7 @@ function createFunction(string,zz){
 
 
     //actual function
-    tempString = tempString + this.theFunct(array,true);
+    tempString = tempString + this.theFunct(array,true,moreThan45);
 
     //finish
     tempString = tempString + "-1\\right)";
@@ -116,7 +158,17 @@ function createFunction(string,zz){
     console.log(this.string);
   }
 
-  this.theFunct = function(array,finish){
+
+  this.theFunct = function(array,finish,moreThan45){
+    if(moreThan45){
+      return this.theFunctMoreThan45(array,finish);
+    } else {
+      return this.theFunctLessThan45(array,finish);
+    }
+  }
+
+  //function for less than 45 degrees
+  this.theFunctLessThan45 = function(array,finish){
     let tempString = "";
     tempString = tempString + "\\left(";
     tempString = tempString + array[2];
@@ -133,6 +185,29 @@ function createFunction(string,zz){
     tempString = tempString + "}\\right)";
     if(finish){
       tempString = tempString + "-y+1";
+    }
+    tempString = tempString + "\\right)";
+    return tempString;
+  }
+
+  //function for more than 45 degrees
+  this.theFunctMoreThan45 = function(array,finish){
+    let tempString = "";
+    tempString = tempString + "\\left(";
+    tempString = tempString + array[0];
+    tempString = tempString + "+\\left(y-";
+    tempString = tempString + array[2];
+    tempString = tempString + "\\right)\\left(\\frac{";
+    tempString = tempString + array[1];
+    tempString = tempString + "-";
+    tempString = tempString + array[0];
+    tempString = tempString + "}{";
+    tempString = tempString + array[3];
+    tempString = tempString + "-";
+    tempString = tempString + array[2];
+    tempString = tempString + "}\\right)";
+    if(finish){
+      tempString = tempString + "-x+1";
     }
     tempString = tempString + "\\right)";
     return tempString;
